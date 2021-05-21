f4388ca5b32739e8da8cbd9ea723925cb84d49bd
behairedman <67635464+behairedman@users.noreply.github.com>
2021-05-21 20:24:17 +0200

    Update ExcelRead.ps1

--- ExcelRead.ps1
+++ ExcelRead.ps1
@@ -11,7 +11,7 @@ $WorkSheet = $WorkBook.sheets.item($Sheet)
 #Erstellen des Datensatzobjekts und DatenListe
 $DatensatzListe = New-Object -TypeName "System.Collections.ArrayList"
 $Datensatz = [PSCustomObject]@{}
-$header = 1   #Zeile mit Attributnamen
+$headerRow = 1   #Zeile mit Attributnamen
 $row = 2 #erste Datenzeile
 $column = 1 #Startspalte
 $rowrange = 6 #Letzte Zeile
@@ -23,7 +23,7 @@ while($row -lt $rowrange)
     $Datensatz = [PSCustomObject]@{}   
     while($column -lt $columnrange)
     {
-        $header = $WorkSheet.Cells.Item($header,$column).Text
+        $header = $WorkSheet.Cells.Item($headerRow,$column).Text
         $Value = $WorkSheet.Cells.Item($row,$column).Text
         $Datensatz | Add-Member -MemberType NoteProperty -Name "$header" -Value "$value"
         $column++
@@ -34,4 +34,5 @@ while($row -lt $rowrange)
     $column = 1
 }
 
+Stop-Process -Name EXCEL
 #END
